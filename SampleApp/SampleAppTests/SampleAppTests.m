ef34d13b0ff4aeb8b4954f66ea0b4f55fb786147
Johannes Plunien <plu@pqpq.de>
2016-02-15 15:28:09 +0400

    Add test target

---
+++ SampleApp/SampleAppTests/SampleAppTests.m
@@ -0,0 +1,17 @@
+//
+//  SampleAppTests.m
+//  SampleAppTests
+//
+//  Created by Johannes Plunien on 15/02/16.
+//  Copyright © 2016 Johannes Plunien. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SampleAppTests : XCTestCase
+
+@end
+
+@implementation SampleAppTests
+
+@end
